30909923c72ab31485c54fa5ff28a62b36b96e40
ASIVEMNQUMENI <97699149+ASIVEMNQUMENI@users.noreply.github.com>
2025-02-13 22:06:43 +0200

    Bonus Task

--- app_folder/clip_model.py
+++
@@ -1,6 +0,0 @@
-from transformers import CLIPProcessor, CLIPModel
-import torch
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
--- app_folder/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-class Config:
-    IMAGE_FOLDER = 'Images'
